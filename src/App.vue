397fcfc0de4060f20614cfeab8355e86ea1a0653
Yoyo Young <yoyoys@livemail.tw>
2018-06-29 19:17:15 +0800

    元件拆分

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,7 @@
     <router-view/>
   </div>
 </template>
+
+<style src="animate.css/animate.min.css"></style>
 <style lang="scss" src="./assets/scss/bootstrap.scss"></style>
 <style lang="scss" src="./assets/scss/font-awesome.scss"></style>
--- src/layouts/backend/Index.vue
+++ src/layouts/backend/Index.vue
@@ -1,51 +1,30 @@
 <template>
 <div class="skin-1">
-    <div id="wrapper">
-        <NavbarContent/>
-
-        <div id="page-wrapper" class="gray-bg">
-            <div class="row border-bottom">
-                <nav class="navbar navbar-static-top white-bg" role="navigation" style="margin-bottom: 0">
-                    <div class="navbar-header">
-                        <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
-                        <form role="search" class="navbar-form-custom" method="post" action="#">
-                            <div class="form-group">
-                                <input type="text" placeholder="Search for something..." class="form-control" name="top-search" id="top-search">
-                            </div>
-                        </form>
-                    </div>
-                    <ul class="nav navbar-top-links navbar-right">
-                        <li>
-                            <a href="#">
-                                <i class="fa fa-sign-out"></i> Log out
-                            </a>
-                        </li>
-                    </ul>
-
-                </nav>
-            </div>
-            <div class="wrapper wrapper-content animated fadeInRight">
-            <router-view/>
-            </div>
-            <div class="footer">
-                <div class="pull-right">
-                    10GB of <strong>250GB</strong> Free.
-                </div>
-                <div>
-                    <strong>Copyright</strong> Example Company &copy; 2014-2018
-                </div>
-            </div>
-
+  <div id="wrapper">
+    <NavbarContent/>
+      <div id="page-wrapper" class="gray-bg">
+        <TopNavbar/>
+        <div class="wrapper wrapper-content">
+          <router-view/>
         </div>
-    </div>
+        <FooterContent/>
+      </div>
+  </div>
 </div>
 </template>
+
 <script lang="ts">
 import Vue from 'vue'
 import NavbarContent from './nav/Index.vue'
+import FooterContent from './footer/Index.vue'
+import TopNavbar from './top-navbar/Index.vue'
 
 export default Vue.extend({
-  components: { NavbarContent }
+  components: {
+    NavbarContent,
+    TopNavbar,
+    FooterContent
+  }
 })
 </script>
 <style lang="scss" src="@/assets/inspania/style.scss"></style>
