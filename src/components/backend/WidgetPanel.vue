b1b2c74a6aedf889161129257d9020030873001a
yoyoys <yoyoys@Yoyos-MacBook-Pro.local>
2018-10-06 17:14:18 +0800

    面板完成

---
+++ src/components/backend/WidgetPanel.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="ibox ">
+    <div class="ibox-title">
+      <h5>{{title}}</h5>
+    </div>
+    <div :class="{'no-padding': noPadding}" class="ibox-content">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    title: {
+      type: String
+    },
+    noPadding: {
+      type: Boolean,
+      default: false
+    }
+  }
+})
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<template lang="pug">
+  .row.about
+    //- h1 This is an about page
+    .col-lg-8
+      WidgetPanel(title="test")
+        h3 test
+    .col-lg-4
+      WidgetPanel(title="test")
+        h3 test
+    .col-lg-4
+      WidgetPanel(title="test")
+        h3 test
+    .col-lg-8
+      WidgetPanel(title="test")
+        h3 test
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import WidgetPanel from '@/components/backend/WidgetPanel.vue'
+
+export default Vue.extend({
+  components: {
+    WidgetPanel
+  }
+})
+</script>
